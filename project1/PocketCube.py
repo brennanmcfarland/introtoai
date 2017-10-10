ee2b873a503efb98880e1569b9e29c05d797dc5d
Brennan McFarland <bfm21@case.edu>
2017-10-10 14:23:17 -0400

    started pocketcube puzzle implementation

---
+++ project1/PocketCube.py
@@ -0,0 +1,37 @@
+import copy
+
+import SearchAlgorithm as Search
+import math
+
+class PocketCube(Search.NodeStateData):
+
+
+    def __eq__(self, other):
+        pass
+
+    def last_move(self):
+        pass
+
+    def __lt__(self, other):
+        pass
+
+    def goal_test(self):
+        pass
+
+    def gcost(self):
+        pass
+
+    def parent(self):
+        pass
+
+    def h2cost(self):
+        pass
+
+    def __hash__(self):
+        pass
+
+    def h1cost(self):
+        pass
+
+    def neighbors(self):
+        pass

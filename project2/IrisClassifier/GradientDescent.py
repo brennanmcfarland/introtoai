163e4e3f7d7f4d5c137d813d7e5271c85b0379d2
Brennan McFarland <bfm21@case.edu>
2017-12-09 15:58:31 -0500

    gradient descent starts from random values, just need to do the writeup now

--- project2/IrisClassifier/GradientDescent.py
+++ project2/IrisClassifier/GradientDescent.py
@@ -1,6 +1,7 @@
 import IrisDataPlotter as iris
 import Boundary as boundary
 import matplotlib.pyplot as pyplot
+import random
 
 # ASSIGNMENT 3.
 iris.load_iris_data()
@@ -8,7 +9,9 @@ data_vectors = [[iris.versicolor_xs[d], iris.versicolor_ys[d]] for d in range(le
                + [[iris.virginica_xs[d], iris.virginica_ys[d]] for d in range(len(iris.virginica_xs))]
 correct_classification = [iris.Classification.VERSICOLOR for c in range(len(iris.versicolor_xs))]\
                          + [iris.Classification.VIRGINICA for c in range(len(iris.virginica_xs))]
-boundary.weights = [2, -2]
+
+random.seed = 2083
+boundary.weights = [random.uniform(0, 10), random.uniform(-10, 0)]
 error_plot_values = []
 iteration = 0
 while True:
